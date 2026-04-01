64d1dd005d3c3731da953695746633756016acff
Logan Jones <emberscreen@gmail.com>
2026-04-01 14:00:08 -0600

    Added: Funtionality of the page view snapping to the top when changing to a new page.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,28 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect, useLayoutEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
 
 function Layout() {
+  const location = useLocation()
+
+  useEffect(() => {
+    if (!window.history.scrollRestoration) {
+      return undefined
+    }
+
+    const previousScrollRestoration = window.history.scrollRestoration
+    window.history.scrollRestoration = 'manual'
+
+    return () => {
+      window.history.scrollRestoration = previousScrollRestoration
+    }
+  }, [])
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location.key])
+
   return (
     <div className="site-shell">
       <a className="skip-link" href="#main-content">
